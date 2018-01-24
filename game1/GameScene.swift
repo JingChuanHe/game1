ecdb42f903248d752cc719f54d8462f769ca9826
jing <jing@jingdeMacBook-Pro.local>
2018-01-24 01:59:26 -0800

    学习iOS游戏

--- game1/GameScene.swift
+++ game1/GameScene.swift
@@ -8,82 +8,165 @@
 
 import SpriteKit
 import GameplayKit
-
+/*
+ 表示图像的类叫做材质
+ */
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let zombie = SKSpriteNode(imageNamed:"zombie1")
+    
+    // 精灵移动初始设置
+    let zombieMovePointsPerSet:CGFloat = 480.0
+    var velocity = CGPoint.zero
+    
+    // 记录相邻两次的点击时间
+    var lastUpdateTime:TimeInterval = 0
+    var dt:TimeInterval = 0
+    
+    // 长按暂停
+    var isClear:Bool = false
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //长按监听
+        let longPress = UILongPressGestureRecognizer(target:self,
+                                                     action:#selector(longPressDid(_:)))
+        self.view?.addGestureRecognizer(longPress)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed:"background1")//,name
+        background.position = CGPoint(x:size.width/2, y:size.height/2)
+        background.zPosition = -1
+        addChild(background)
+        
+        zombie.position = CGPoint(x:400, y:400)
+        addChild(zombie)
+        
+    }
+    
+    @objc func longPressDid(_ sender: UILongPressGestureRecognizer){
+        
+        
+        isClear = !isClear
+    }
+    
+    func moveZombieToward(location:CGPoint){
+        
+        let offset = CGPoint(x:location.x - zombie.position.x,y:location.y - zombie.position.y)
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        
+        let direction = CGPoint(x:offset.x / CGFloat(length),y:offset.y / CGFloat(length))
+        velocity = CGPoint(x:direction.x * zombieMovePointsPerSet,y:direction.y * zombieMovePointsPerSet)
+        
+        
+    }
+    
+    // 记录每次更新的时间
+    override func update(_ currentTime: TimeInterval) {
+        print("update····,\(currentTime)")
+        if isClear {
+            velocity = CGPoint.zero
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            lastUpdateTime = 0
+            dt = 0
+            zombie.position = CGPoint(x:400, y:400)
+        }else{
+            if lastUpdateTime > 0 {
+                
+                dt = currentTime - lastUpdateTime
+            }else{
+                dt = 0
+            }
+            
+            lastUpdateTime = currentTime
+            moveSprite(sprite: zombie, velocity: velocity)
+            boundCheckZombie()
+            roateSpriate(sprite: zombie, direction: velocity)
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+    }
+    
+    func moveSprite(sprite:SKSpriteNode ,velocity:CGPoint){
+        
+        let amountToMove = CGPoint(x:velocity.x * CGFloat(dt),y:velocity.y * CGFloat(dt))
+        
+        sprite.position = CGPoint(x: sprite.position.x+amountToMove.x, y: sprite.position.y + amountToMove.y)
+    }
+    
+    func sceneTouched(touchLoaction:CGPoint){
+        moveZombieToward(location:touchLoaction)
+    }
+    
+    // 记录每次的事件
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+//        isClear = !isClear
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLoaction: touchLocation)
+        
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let touch = touches.first else {
+            return
+        }
+        
+//        isClear = !isClear
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLoaction: touchLocation)
+    }
+ 
+    
+    
+    
+    // 检测边界
+    func boundCheckZombie(){
+        
+        let bottomLeft = CGPoint.zero
+        let topRight = CGPoint(x:size.width,y:size.height)
+        
+        if zombie.position.x <= bottomLeft.x {
+            zombie.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.y <= bottomLeft.y {
+            zombie.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        
+        if zombie.position.x >= topRight.x {
+            zombie.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.y >= topRight.y {
+            zombie.position.y = topRight.y
+            velocity.y = -velocity.y
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    // 旋转精灵
+    func roateSpriate(sprite:SKSpriteNode,direction:CGPoint){
+        
+        UIView.animate(withDuration: 0.25) {
+            
+            sprite.zRotation = CGFloat(atan2(Double(direction.y),Double(direction.x)))
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
--- game1/GameViewController.swift
+++ game1/GameViewController.swift
@@ -8,48 +8,32 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let scence = GameScene(size:CGSize(width:2048,height:1536))
+        
+        let skView = self.view as! SKView
+        
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        scence.scaleMode = .aspectFill
+        skView.presentScene(scence)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
+    //隐藏状态栏
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    
+
+    
 }
